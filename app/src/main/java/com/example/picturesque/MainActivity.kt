033d75a5f9667c676c39f32ffdb3dc6d81772d91
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-16 21:26:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/picturesque/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.picturesque
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
