b54255c998e998c46d9bf7669d921dd59dcf4f52
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-17 20:34:24 +0200

    progress update

--- app/src/main/java/com/example/picturesque/DrawingView.kt
+++ app/src/main/java/com/example/picturesque/DrawingView.kt
@@ -10,7 +10,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
     private var mDrawPath: CustomPath? = null
     private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
-    private var canvasPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
@@ -26,13 +26,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         mDrawPaint!!.style = Paint.Style.STROKE
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
     }
 
 
 
 
-    internal inner class CustomPath(val color: Int, var brushThickness: Float) : Path{
+    internal inner class CustomPath(val color: Int, var brushThickness: Float) : Path(){
 
     }
 
