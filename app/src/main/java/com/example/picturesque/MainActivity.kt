e9b2be42191ac9da55eb528f4d4a81b52fc54e1e
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-19 22:35:20 +0200

    progress update

--- app/src/main/java/com/example/picturesque/MainActivity.kt
+++ app/src/main/java/com/example/picturesque/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.picturesque
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,27 @@ class MainActivity : AppCompatActivity() {
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
+        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+
     }
+
+    fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBtn = brushDialog.findViewById(R.id.ib_small_brush) as ImageButton
+        smallBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
+
+
+    }
+
 }
