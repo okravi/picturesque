ac96518dbb5bf8cbdb7e2ef0d2919f047576d0a4
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-20 20:50:32 +0200

    progress update

--- app/src/main/java/com/example/picturesque/MainActivity.kt
+++ app/src/main/java/com/example/picturesque/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.picturesque
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -60,6 +62,23 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
 
 
+    }
+
+    fun paintClicked(view: View){
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 
 }
