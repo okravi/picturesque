9ad9c8f45d3a96dc700a2be459d7354b31b694f9
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2021-11-24 21:59:51 +0200

    progress update

--- app/src/main/java/com/example/picturesque/MainActivity.kt
+++ app/src/main/java/com/example/picturesque/MainActivity.kt
@@ -4,6 +4,9 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
@@ -167,6 +170,22 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width,
+            view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+        return returnedBitmap
+    }
+
 
 
 
